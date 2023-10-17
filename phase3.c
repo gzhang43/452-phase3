9b6963d193cbf1061943bc329c9c1199e4dd92e4
Grace Zhang <gracezhang@Graces-MacBook-Pro.local>
2023-10-16 17:31:35 -0700

    add starter files

---
+++ phase3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <usloss.h>
+#include "phase1.h"
+#include "phase2.h"
+#include "phase3.h"
+
+void phase3_init(void) {
+
+}
+
+void phase3_start_service_processes(void) {
+
+}
