8c8aec560b6acf8b89499251d16d9b12a1059c15
EllieVM <87343586+EllieVM@users.noreply.github.com>
2023-10-17 16:44:58 -0700

    added PCB struct

--- phase3.c
+++ phase3.c
@@ -7,6 +7,15 @@
 
 int(*func)(char*);
 
+typedef struct PCB {
+    char* name;
+    int (*startFunc)(char*);
+    char* arg;
+    int pid;
+    int priority;
+    int status;
+} PCB;
+
 void kernSpawn(USLOSS_Sysargs *arg);
 void kernWait(USLOSS_Sysargs *arg);
 void kernTerminate(USLOSS_Sysargs *arg);
@@ -36,14 +45,15 @@ void kernSpawn(USLOSS_Sysargs *arg) {
     func = (int(*)(char*))arg->arg1;
     int stackSize = (int)(long)arg->arg3;
     int priority = (int)(long)arg->arg4;
+
     int ret = fork1(arg->arg5, trampolineFunc, arg->arg2, stackSize, priority);
 
     arg->arg1 = (void*)(long)ret;
 }
 
 void kernWait(USLOSS_Sysargs *arg) {
-    int *status = (int*)(long)arg->arg2;
-    int ret = join(status);
+    //int *status = (int*)(long)arg->arg2;
+    int ret = join((int*)(long)arg->arg2);
     
     if (ret == -2) {
         arg->arg4 = (void*)(long)-2;
@@ -51,7 +61,7 @@ void kernWait(USLOSS_Sysargs *arg) {
     else {
         arg->arg4 = (void*)(long)0;
         arg->arg1 = (void*)(long)ret;
-        arg->arg2 = (void*)(long)status;
+        //arg->arg2 = (void*)(long)status;
     }
 }
 
